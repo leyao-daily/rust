0264865b2b80b9d9b45996df717f5f7a1ff1a68e
Yao Le <le.yao@intel.com>
2019-11-12 03:42:35 -0500

    Update

---
+++ project/guessing_game/target/debug/deps/guessing_game-bb3da82674df37eb.d
@@ -0,0 +1,5 @@
+/root/rust/project/guessing_game/target/debug/deps/guessing_game-bb3da82674df37eb: src/main.rs
+
+/root/rust/project/guessing_game/target/debug/deps/guessing_game-bb3da82674df37eb.d: src/main.rs
+
+src/main.rs:
---
+++ project/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/root/rust/project/guessing_game/target/debug/guessing_game: /root/rust/project/guessing_game/src/main.rs
