a99d525add49640e6b08edba906fbe7864efb817
Yao Le <le.yao@intel.com>
2019-11-26 20:53:48 -0500

    struct

--- project/struct_test/src/main.rs
+++ project/struct_test/src/main.rs
@@ -9,6 +9,12 @@ struct User {
 fn main() {
     let mut user1 = build_user("le.yao@intel.com".to_string(), "Yao Le".to_string());
     println!("{}", user1.email);
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("another"),
+        ..user1
+    };
+    println!("{}", user2.username);
 }
 
 
