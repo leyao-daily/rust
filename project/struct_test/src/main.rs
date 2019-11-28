5f774da410a26982025514940b9e1f75bf2cdefc
Yao Le <le.yao@intel.com>
2019-11-28 03:22:33 -0500

    struct

--- project/struct_test/src/main.rs
+++ project/struct_test/src/main.rs
@@ -15,6 +15,13 @@ fn main() {
         ..user1
     };
     println!("{}", user2.username);
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
 }
 
 
