e7d07c34b7ec487d3adf1c702493f77c4aec8df9
Yao Le <le.yao@intel.com>
2019-12-02 01:44:25 -0500

    rectangles

--- project/rectangles/src/main.rs
+++ project/rectangles/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
 }
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
