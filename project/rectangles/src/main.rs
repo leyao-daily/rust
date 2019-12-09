a69f4118add462ddcda4a824ff84fc22a0a64035
Yao Le <le.yao@intel.com>
2019-12-09 01:48:58 -0500

    rectangles

--- project/rectangles/src/main.rs
+++ project/rectangles/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
     println!("rect1 is {:?}", rect1);
-
+    println!("rect1 is {:#?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
