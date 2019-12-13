c342e4ae70020c625df616487ce69f40e125a081
Yao Le <le.yao@intel.com>
2019-12-12 21:22:05 -0500

    rectangle

--- project/rectangles/src/main.rs
+++ project/rectangles/src/main.rs
@@ -8,10 +8,20 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
@@ -19,5 +29,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
