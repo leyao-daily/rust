aa9a60597b5228ef45330ed63f754bb5a06be94f
Yao Le <le.yao@intel.com>
2019-12-15 19:59:33 -0500

    rectangles

--- project/rectangles/src/main.rs
+++ project/rectangles/src/main.rs
@@ -22,13 +22,19 @@ fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
+    let rect4 = Rectangle::square(35);
 
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
     println!(
-        "The area of the rectangle is {} square pixels.",
+        "The area of the rectangle 1 is {} square pixels.",
         rect1.area()
     );
+    println!(
+        "The area of the rectangle 4 is {} square pixels.",
+        rect4.area()
+    );
+
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
