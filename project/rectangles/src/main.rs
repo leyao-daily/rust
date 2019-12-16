230077832648fa3dd0a41f9a6f258eda935faf70
Yao Le <le.yao@intel.com>
2019-12-16 01:31:33 -0500

    rectangles

--- project/rectangles/src/main.rs
+++ project/rectangles/src/main.rs
@@ -9,12 +9,14 @@ impl Rectangle {
         self.width * self.height
     }
 
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
     fn square(size: u32) -> Rectangle {
         Rectangle { width: size, height: size }
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
